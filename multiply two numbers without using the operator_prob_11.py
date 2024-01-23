d959c8ff510421d94705c9bf6d336b8bc68b2114
Proshenzit Roy <37993972+Proshenzit@users.noreply.github.com>
2024-01-23 08:57:07 -0800

    Add files via upload

---
+++ multiply two numbers without using the operator_prob_11.py
@@ -0,0 +1,6 @@
+first_num= int(input("enter your first nunber: "))
+sec_num=int (input("enter  your  second number: "))
+sum = 0
+for  i in   range(0,first_num):
+  sum =sum+sec_num
+print("Your reslut is:",sum)
